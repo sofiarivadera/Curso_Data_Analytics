3789dc5ae601ae31f8b95dee106de1fac8286f96
sofiarivadera <sofia.rivadera@gmail.com>
2026-09-04 22:14:28 -0300

    Update and rename outer-joins-ministore to soluciones.sql

--- soluciones.sql
+++ soluciones.sql
@@ -5,8 +5,7 @@
 -- ══════════════════════════════════════════
 -- ── CONSULTA 1: LEFT JOIN ─────────────────
 -- Pregunta de negocio: ¿Qué productos del catálogo nunca fueron vendidos?
--- Mostrá todos los productos y sus ventas asociadas.
--- Los productos sin ventas aparecerán con NULL en las columnas de ventas.
+
 SELECT 
     p.producto_id,
     p.nombre AS producto,
@@ -14,25 +13,30 @@ SELECT
     v.venta_id,
     v.fecha_venta
 FROM productos p
-LEFT JOIN ventas v ON p.producto_id = v.producto_id;
+LEFT JOIN ventas v ON p.producto_id = v.producto_id
+WHERE v.venta_id IS NULL;
+
 -- ── CONSULTA 2: RIGHT JOIN ────────────────
 -- Pregunta de negocio: ¿Existen ventas registradas con productos que no figuran en nuestro catálogo? (posible error de carga de datos)
--- Los registros huérfanos aparecerán con NULL en las columnas de productos.
+
+SELECT 
+    p.producto_id AS producto_catalogo_id,
+    p.nombre AS producto,
+    p.categoria,
+    v.venta_id,
+    v.producto_id AS venta_producto_id,
+    v.fecha_venta
+FROM productos p
+RIGHT JOIN ventas v ON p.producto_id = v.producto_id
+WHERE p.producto_id IS NULL;
+
+-- ── CONSULTA 3: FULL OUTER JOIN ───────────
+-- Pregunta de negocio: Vista completa de auditoría que muestre todos los productos y todas las ventas sin perder ninguna fila, identificando tanto productos sin ventas como ventas sin producto.
 SELECT 
     p.producto_id,
     p.nombre AS producto,
     p.categoria,
-    v.venta_id,
+    v.venta_id AS ventas,
     v.fecha_venta
 FROM productos p
-RIGHT JOIN ventas v ON p.producto_id = v.producto_id;
--- ── CONSULTA 3: FULL OUTER JOIN ───────────
--- Pregunta de negocio: Vista completa de auditoría que muestre todos los productos y todas las ventas sin perder ninguna fila, identificando tanto productos sin ventas como ventas sin producto.
-SELECT
-p.producto_id,
-p.nombre AS producto,
-p.categoria,
-v.venta_id AS ventas,
-v.fecha_venta
-FROM Productos p
 FULL OUTER JOIN ventas v ON p.producto_id = v.producto_id;
